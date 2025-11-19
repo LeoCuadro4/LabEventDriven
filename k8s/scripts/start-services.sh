329c782a847d30d89d5f690fd3c0cbc7ef5504c5
MiguelSalcedo225 <miguelangelsalcedo2020@gmail.com>
2025-11-19 13:04:33 -0500

    Inventory-Service

--- k8s/scripts/start-services.sh
+++ k8s/scripts/start-services.sh
@@ -27,6 +27,13 @@ echo "--- Waiting for Product Service ---"
 # FIX: Do not use 'pod --all'. Target the specific app label instead.
 kubectl wait --for=condition=ready pod -l app=product-service -n microservices --timeout=180s
 
+
+echo "--- Deploying Inventory Service ---"
+kubectl apply -f ../services/inventory-service.yml
+
+echo "--- Waiting for Inventory Service ---"
+kubectl wait --for=condition=ready pod -l app=inventory-service -n microservices --timeout=180s
+
 echo "--- Deploying Shipping Service ---"
 kubectl apply -f ../services/shipping-service.yml
 
@@ -41,8 +48,10 @@ echo "--- Port Forwarding ---"
  
 kubectl port-forward -n microservices svc/user-service 3001:3001 &
 kubectl port-forward -n microservices svc/product-service 3002:3002 &
+kubectl port-forward -n microservices svc/inventory-service 3005:3005 &
 kubectl port-forward -n microservices svc/shipping-service 3006:3006 &
 
 echo "Deployment complete. User Service accessible at localhost:3001"
 echo "Product Service accessible at localhost:3002"
+echo "Inventory Service accessible at localhost:3005"
 echo "Shipping Service accessible at localhost:3006"
